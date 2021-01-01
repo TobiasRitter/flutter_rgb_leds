878a2a5b58c337fa1afd24016205c1f3547e2b62
TobiasRitter <tobias.ritter98@gmail.com>
2021-01-01 18:13:28 +0100

    use shared prefs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -37,22 +38,18 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
 
-  final Map<String, String> presets = {
-    "Preset 1": "FF0000",
-    "Preset 2": "00FF00",
-    "Preset 3": "0000FF",
-  };
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<SharedPreferences> prefs = SharedPreferences.getInstance();
+  Future<Map<String, String>> presets;
   bool editing = false;
   Color color;
   int alpha = 255;
   String lastValidRgb;
-  TextEditingController colorController;
+  TextEditingController colorController = TextEditingController();
   String dropdownValue = "Custom";
 
   Color getRgbColor(String rgb) {
@@ -66,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
     print("broadcast: $rgb");
   }
 
-  void submitCol(String rgb, BuildContext context) {
+  void submitCol(String rgb, BuildContext context) async {
     try {
       var col = getRgbColor(rgb).withAlpha(alpha);
       setState(() {
@@ -74,6 +71,11 @@ class _MyHomePageState extends State<MyHomePage> {
         colorController.text = rgb;
         lastValidRgb = rgb;
       });
+      if (dropdownValue != "Custom" && !editing) {
+        var _prefs = await prefs;
+        _prefs.setString(dropdownValue, lastValidRgb);
+        print("Setting $dropdownValue to $lastValidRgb");
+      }
       broadcastCol(color);
     } catch (e) {
       colorController.text = lastValidRgb;
@@ -88,8 +90,14 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    colorController = TextEditingController();
     submitCol("FFFFFF", context);
+    presets = prefs.then(
+      (value) => {
+        "Preset 1": value.getString("Preset 1") ?? "FF0000",
+        "Preset 2": value.getString("Preset 2") ?? "00FF00",
+        "Preset 3": value.getString("Preset 3") ?? "0000FF",
+      },
+    );
   }
 
   @override
@@ -100,160 +108,183 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    List<String> options = ["Custom"] + widget.presets.keys.toList();
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: RadialGradient(
-                  radius: 0.5,
-                  colors: [
-                    color.withAlpha((sqrt(alpha / 255) * 255 * 0.7).round()),
-                    Colors.transparent
-                  ],
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.lightbulb_outline,
-                    size: 128,
-                    color: color.withAlpha(255),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(48),
-            child: Column(
-              children: <Widget>[
-                Column(
+      body: Center(
+        child: FutureBuilder<Map<String, String>>(
+          future: presets,
+          builder: (context, presetsSnapshot) {
+            switch (presetsSnapshot.connectionState) {
+              case ConnectionState.waiting:
+                return const CircularProgressIndicator();
+              default:
+                return Column(
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          width: 48,
-                          height: 48,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 24),
-                          child: DropdownButton<String>(
-                            underline: Container(),
-                            value: dropdownValue,
-                            onChanged: (val) {
-                              setState(() {
-                                dropdownValue = val;
-                                editing = false;
-                              });
-                              if (widget.presets.containsKey(dropdownValue)) {
-                                submitCol(
-                                    widget.presets[dropdownValue], context);
-                              } else {
-                                submitCol("FFFFFF", context);
-                              }
-                            },
-                            items: options
-                                .map(
-                                  (k) => DropdownMenuItem(
-                                    value: k,
-                                    child: Center(child: Text(k)),
-                                  ),
-                                )
-                                .toList(),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: RadialGradient(
+                            radius: 0.5,
+                            colors: [
+                              color.withAlpha(
+                                  (sqrt(alpha / 255) * 255 * 0.7).round()),
+                              Colors.transparent
+                            ],
                           ),
                         ),
-                        widget.presets.containsKey(dropdownValue) && !editing
-                            ? IconButton(
-                                icon: Icon(Icons.edit_outlined),
-                                onPressed: () => setState(() => editing = true),
-                              )
-                            : editing
-                                ? IconButton(
-                                    icon: Icon(Icons.done),
-                                    onPressed: () {
-                                      setState(() => editing = false);
-                                      submitCol(colorController.text, context);
-                                    })
-                                : Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.lightbulb_outline,
+                              size: 128,
+                              color: color.withAlpha(255),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(48),
+                      child: Column(
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
                                     width: 48,
                                     height: 48,
                                   ),
-                      ],
-                    ),
-                    !widget.presets.containsKey(dropdownValue) || editing
-                        ? Padding(
-                            padding: const EdgeInsets.only(top: 16),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 24),
+                                    child: DropdownButton<String>(
+                                      underline: Container(),
+                                      value: dropdownValue,
+                                      onChanged: (val) {
+                                        setState(() {
+                                          dropdownValue = val;
+                                          editing = false;
+                                        });
+                                        if (dropdownValue != "Custom") {
+                                          submitCol(
+                                              presetsSnapshot.data[dropdownValue],
+                                              context);
+                                        } else {
+                                          submitCol("FFFFFF", context);
+                                        }
+                                      },
+                                      items: (["Custom"] +
+                                              presetsSnapshot.data.keys.toList())
+                                          .map(
+                                            (k) => DropdownMenuItem(
+                                              value: k,
+                                              child: Center(child: Text(k)),
+                                            ),
+                                          )
+                                          .toList(),
+                                    ),
+                                  ),
+                                  dropdownValue != "Custom" && !editing
+                                      ? IconButton(
+                                          icon: Icon(Icons.edit_outlined),
+                                          onPressed: () =>
+                                              setState(() => editing = true),
+                                        )
+                                      : editing
+                                          ? IconButton(
+                                              icon: Icon(Icons.done),
+                                              onPressed: () {
+                                                setState(() => editing = false);
+                                                submitCol(colorController.text,
+                                                    context);
+                                              })
+                                          : Container(
+                                              width: 48,
+                                              height: 48,
+                                            ),
+                                ],
+                              ),
+                              dropdownValue == "Custom" || editing
+                                  ? Padding(
+                                      padding: const EdgeInsets.only(top: 16),
+                                      child: Row(
+                                        children: [
+                                          Expanded(
+                                            child: TextField(
+                                              textAlign: TextAlign.center,
+                                              controller: colorController,
+                                              decoration: InputDecoration(
+                                                enabledBorder:
+                                                    OutlineInputBorder(
+                                                  borderSide: BorderSide(
+                                                      color: Colors.white38,
+                                                      width: 1),
+                                                  borderRadius:
+                                                      BorderRadius.circular(50),
+                                                ),
+                                                focusedBorder:
+                                                    OutlineInputBorder(
+                                                  borderSide: BorderSide(
+                                                      color: Colors.white,
+                                                      width: 1),
+                                                  borderRadius:
+                                                      BorderRadius.circular(50),
+                                                ),
+                                                labelText: "#",
+                                              ),
+                                              onSubmitted: (val) {
+                                                submitCol(val, context);
+                                              },
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  : Container(),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 32),
                             child: Row(
                               children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(12),
+                                  child: Icon(
+                                    Icons.nights_stay_outlined,
+                                    color: Colors.white38,
+                                  ),
+                                ),
                                 Expanded(
-                                  child: TextField(
-                                    textAlign: TextAlign.center,
-                                    controller: colorController,
-                                    decoration: InputDecoration(
-                                      enabledBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                            color: Colors.white38, width: 1),
-                                        borderRadius: BorderRadius.circular(50),
-                                      ),
-                                      focusedBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(
-                                            color: Colors.white, width: 1),
-                                        borderRadius: BorderRadius.circular(50),
-                                      ),
-                                      labelText: "#",
-                                    ),
-                                    onSubmitted: (val) {
-                                      submitCol(val, context);
-                                    },
+                                  child: Slider(
+                                      value: alpha / 255,
+                                      onChanged: (val) {
+                                        setState(() {
+                                          alpha = (val * 255).round();
+                                          color = color.withAlpha(alpha);
+                                        });
+                                        broadcastCol(color);
+                                      }),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(12),
+                                  child: Icon(
+                                    Icons.wb_sunny_outlined,
+                                    color: Colors.white38,
                                   ),
                                 ),
                               ],
                             ),
                           )
-                        : Container(),
+                        ],
+                      ),
+                    ),
                   ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 32),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(12),
-                        child: Icon(
-                          Icons.nights_stay_outlined,
-                          color: Colors.white38,
-                        ),
-                      ),
-                      Expanded(
-                        child: Slider(
-                            value: alpha / 255,
-                            onChanged: (val) {
-                              setState(() {
-                                alpha = (val * 255).round();
-                                color = color.withAlpha(alpha);
-                              });
-                              broadcastCol(color);
-                            }),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(12),
-                        child: Icon(
-                          Icons.wb_sunny_outlined,
-                          color: Colors.white38,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
+                );
+            }
+          },
+        ),
       ),
     );
   }
