05dbbb256f9a545d6e3becb1e24edf953d84fb7d
TobiasRitter <tobias.ritter98@gmail.com>
2021-01-02 10:32:03 +0100

    extract hex field widget

--- lib/main.dart
+++ lib/main.dart
@@ -254,43 +254,9 @@ class _MyHomePageState extends State<MyHomePage> {
                                     ],
                                   ),
                                   dropdownValue == "Custom" || editing
-                                      ? Padding(
-                                          padding:
-                                              const EdgeInsets.only(top: 16),
-                                          child: Row(
-                                            children: [
-                                              Expanded(
-                                                child: TextField(
-                                                  textAlign: TextAlign.center,
-                                                  controller: colorController,
-                                                  decoration: InputDecoration(
-                                                    enabledBorder:
-                                                        OutlineInputBorder(
-                                                      borderSide: BorderSide(
-                                                          color: Colors.white60,
-                                                          width: 1),
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                              50),
-                                                    ),
-                                                    focusedBorder:
-                                                        OutlineInputBorder(
-                                                      borderSide: BorderSide(
-                                                          color: Colors.white,
-                                                          width: 1),
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                              50),
-                                                    ),
-                                                    labelText: "#",
-                                                  ),
-                                                  onSubmitted: (val) {
-                                                    submitCol(val, context);
-                                                  },
-                                                ),
-                                              ),
-                                            ],
-                                          ),
+                                      ? HexField(
+                                          colorController: colorController,
+                                          onSubmitted: submitCol,
                                         )
                                       : Container(),
                                 ],
@@ -337,6 +303,48 @@ class _MyHomePageState extends State<MyHomePage> {
             },
           ),
         ),
+      ),
+    );
+  }
+}
+
+class HexField extends StatelessWidget {
+  const HexField({
+    Key key,
+    @required this.colorController,
+    @required this.onSubmitted,
+  }) : super(key: key);
+
+  final TextEditingController colorController;
+  final Function(String, BuildContext) onSubmitted;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              textAlign: TextAlign.center,
+              controller: colorController,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white60, width: 1),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white, width: 1),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                labelText: "#",
+              ),
+              onSubmitted: (val) {
+                onSubmitted(val, context);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
